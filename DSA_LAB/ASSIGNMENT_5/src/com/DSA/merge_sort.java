999485dc074e2b3bd03d526d9e0790970a82c64c
jaffarabbas <gamaportal8@gmail.com>
2020-11-16 08:10:48 +0500

    16-11-2020

--- DSA_LAB/ASSIGNMENT_5/src/com/DSA/merge_sort.java
+++ DSA_LAB/ASSIGNMENT_5/src/com/DSA/merge_sort.java
@@ -5,28 +5,36 @@ import java.util.Arrays;
 public class merge_sort {
     static void merge(int[] arr,int low,int mid,int high){
         int i,j,k;
-        i=low;j=high;k=low;
+        i=low;j=mid+1;k=low;
         int[] temp = new int[high+1];
         while(i<=mid&&j<=high){
             if(arr[i] < arr[j]){
+               // System.out.println("When arr[i] : "+arr[i]+" < arr[j] : "+arr[j]+"\n");
                 temp[k] = arr[i];
                 i++;k++;
+                System.out.println("Temp arr of i : "+Arrays.toString(temp)+" i: "+i+" k :"+k+"");
             }else{
+               // System.out.println("When arr[i] : "+arr[i]+" > arr[j] : "+arr[j]+"\n");
                 temp[k]=arr[j];
                 j++;k++;
+                System.out.println("Temp arr of j : "+Arrays.toString(temp)+" j: "+j+" k :"+k);
             }
-            while (i <= mid) {
-                temp[k]=arr[i];
-                k++;i++;
-            }
-            while(j<=high){
-                temp[k] = arr[j];
-                k++;j++;
-            }
-            for(i=low;i<=high;i++){
-                arr[i] = temp[i];
-            }
+
         }
+        while (i <= mid) {
+            temp[k]=arr[i];
+            k++;i++;
+            System.out.println("Temp arr of reaming i : "+Arrays.toString(temp)+" i: "+i+" k :"+k);
+        }
+        while(j<=high){
+            temp[k] = arr[j];
+            k++;j++;
+            System.out.println("Temp arr of reaming j : "+Arrays.toString(temp)+" i: "+j+" k :"+k);
+        }
+        for(int loop=low;loop<=high;loop++){
+            arr[loop] = temp[loop];
+        }
+        System.out.println(Arrays.toString(arr));
     }
     static void sort(int[] arr,int low,int high){
         int mid;
@@ -38,11 +46,12 @@ public class merge_sort {
         }
     }
     static void test(){
-        int[] arr={5,4,3,2,1};
+        int[] arr={4,8,3,1,6,7};
         int size = arr.length-1;
         System.out.println("Unsorted!!");
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr)+"\n\n**********Dry Run**********\n\n");
         sort(arr,0,size);
+        System.out.println("sorted!!");
         System.out.println(Arrays.toString(arr));
     }
     public static void main(String[] args) {
--- DSA_LAB/ASSIGNMENT_5/src/com/DSA/quick_sort.java
+++ DSA_LAB/ASSIGNMENT_5/src/com/DSA/quick_sort.java
@@ -4,27 +4,31 @@ import java.util.Arrays;
 
 public class quick_sort {
     static int partition(int[] arr,int low,int high){
-        int pivoit,i,j,temp;
+        int pivoit,i,j,temp,count=0;
         pivoit=arr[high];
         i=low-1;
         boolean flag=false;
         for ( j = low; j < high; j++) {
             if(arr[j] < pivoit){
                 i++;
-                System.out.println("i: "+i+" j: "+j);
+                System.out.println("i: "+i+" j: "+j+" arr[i] : ["+arr[i]+"]  arr[j] : ["+arr[j]+"] pivot : ["+arr[high]+"] arr["+arr[j]+"] < arr["+arr[high]+"] swap("+arr[i]+" , "+arr[j]+") \n");
                 temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
                 flag=true;
-                System.out.println(Arrays.toString(arr)+" swap : "+flag);
+                System.out.println(Arrays.toString(arr)+" swap : "+flag+"\n");
                 flag =false;
+            }else{
+                flag =false;
+                System.out.println("i: "+i+" j: "+j+" value : "+arr[j]+" pivot : ["+arr[high]+"]  -> swap : "+flag+"\n");
             }
         }
         temp = arr[i+1];
         arr[i+1] = arr[high];
         arr[high] = temp;
+
         flag = true;
-        System.out.println("swap ("+arr[i+1]+","+arr[high]+" )"+" swap : "+flag);
+        System.out.println("swap ("+arr[i+1]+","+arr[high]+" ) swap : "+flag+"\n");
         return i+1;
     }
     static void sort(int[] arr,int low,int high){
@@ -35,11 +39,12 @@ public class quick_sort {
         }
     }
     static void test(){
-        int[] arr={5,4,3,2,1};
+        int[] arr={4,8,3,1,6,7};
         int size = arr.length-1;
         System.out.println("Unsorted!!");
         System.out.println(Arrays.toString(arr));
         sort(arr,0,size);
+        System.out.println("sorted!!");
         System.out.println(Arrays.toString(arr));
     }
     public static void main(String[] args) {
