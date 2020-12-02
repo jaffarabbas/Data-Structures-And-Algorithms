a0eac9e4354c268a147273e22123218659c8e063
jaffarabbas <gamaportal8@gmail.com>
2020-12-02 09:59:55 +0500

    12-2-2020

--- Data Structures/Stacks/src/com/DSA/stack_implementation_2.java
+++ Data Structures/Stacks/src/com/DSA/stack_implementation_2.java
@@ -1,41 +1,116 @@
-package com.DSA;
+package com.DSA;// Java program to implement a stack that supports
+// getMinimum() in O(1) time and O(1) extra space.
+import java.util.*;
 
+// A user defined stack that supports getMin() in
+// addition to push() and pop()
+class MyStack
+{
+    Stack<Integer> s;
+    Integer minEle;
 
-import java.util.Stack;
+    // Constructor
+    MyStack() { s = new Stack<Integer>(); }
 
-class Stacks{
-    public void stack_push(Stack<String> stack,String value){
-        String check = stack.push(value);
-        System.out.println("Value push : "+check);
+    // Prints minimum element of MyStack
+    void getMin()
+    {
+        // Get the minimum number in the entire stack
+        if (s.isEmpty())
+            System.out.println("Stack is empty");
+
+            // variable minEle stores the minimum element
+            // in the stack.
+        else
+            System.out.println("Minimum Element in the " +
+                    " stack is: " + minEle);
     }
-    public void stack_pop(Stack<String> stack){
-        String value  = stack.pop();
-        System.out.println("Poped value : "+value);
-    }
-    public void stack_peek(Stack<String> stack){
-        String value  = stack.peek();
-        System.out.println("Peek : "+value);
-    }
-    public void print_stack(Stack<String> stack){
-        for (int i = 0; i < stack.size(); i++) {
-            stack_peek(stack);
-            stack_pop(stack);
+
+    // prints top element of MyStack
+    void peek()
+    {
+        if (s.isEmpty())
+        {
+            System.out.println("Stack is empty ");
+            return;
         }
+
+        Integer t = s.peek(); // Top element.
+
+        System.out.print("Top Most Element is: ");
+
+        // If t < minEle means minEle stores
+        // value of t.
+        if (t < minEle)
+            System.out.println(minEle);
+        else
+            System.out.println(t);
     }
-}
 
+    // Removes the top element from MyStack
+    void pop()
+    {
+        if (s.isEmpty())
+        {
+            System.out.println("Stack is empty");
+            return;
+        }
 
+        System.out.print("Top Most Element Removed: ");
+        Integer t = s.pop();
 
-public class stack_implementation_2 {
-    public static void main(String[] args) {
-        Stack<String> stack = new Stack<String>();
-        Stacks obj = new Stacks();
-        obj.stack_push(stack,"dsfdsf");
-        obj.stack_push(stack,"dsfdsf");
-        obj.stack_push(stack,"dsfdsf");
-        obj.stack_push(stack,"dsfdsf");
-        obj.stack_push(stack,"3");
-        obj.stack_peek(stack);
-        obj.print_stack(stack);
+        // Minimum will change as the minimum element
+        // of the stack is being removed.
+        if (t < minEle)
+        {
+            System.out.println(minEle);
+            minEle = 2*minEle - t;
+        }
+
+        else
+            System.out.println(t);
+    }
+
+    // Insert new number into MyStack
+    void push(Integer x)
+    {
+        if (s.isEmpty())
+        {
+            minEle = x;
+            s.push(x);
+            System.out.println("Number Inserted: " + x);
+            return;
+        }
+
+        // If new number is less than original minEle
+        if (x < minEle)
+        {
+            s.push(2*x - minEle);
+            minEle = x;
+        }
+
+        else
+            s.push(x);
+
+        System.out.println("Number Inserted: " + x);
+    }
+};
+
+// Driver Code
+public class stack_implementation_2
+{
+    public static void main(String[] args)
+    {
+        MyStack s = new MyStack();
+        s.push(3);
+        s.push(5);
+        s.getMin();
+        s.push(2);
+        s.push(1);
+        s.getMin();
+        s.pop();
+        s.getMin();
+        s.pop();
+        s.peek();
     }
 }
---
+++ Data_practice_cross_language/java/Stacks/satck1.java
@@ -0,0 +1,11 @@
+
+import java.util.Stack;
+public class satck1 {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        stack.push(33);
+
+        System.out.println(stack);
+    }
+}
