6ef530cf710ea1c0c423305ae860381a20cd4b9b
jaffarabbas <gamaportal8@gmail.com>
2020-12-05 00:10:11 +0500

    12-5-2020

--- DSA_LAB/ASSIGNMENT_6/src/Question_1.java
+++ DSA_LAB/ASSIGNMENT_6/src/Question_1.java
@@ -5,38 +5,29 @@ public class Question_1 {
         Stack<Integer> s1 = new Stack<>();
         Stack<Integer> s2 = new Stack<>();
         Scanner scn = new Scanner(System.in);
-        int key,temp=0,count=0;
+        int key, temp = 0, count = 0;
         boolean flag = false;
-        s1.push(10);
-        s1.push(33);
-        s1.push(4);
-        s1.push(55);
-        s1.push(3);
-        s1.push(50);
-        s1.push(41);
-        s1.push(32);
-        s1.push(29);
-        s1.push(10);
+        s1.push(10); s1.push(33); s1.push(4); s1.push(55); s1.push(3); s1.push(50); s1.push(41); s1.push(32); s1.push(29); s1.push(10);
         System.out.println(s1);
         System.out.println("Your Desired Value to delete : ");
         key = scn.nextInt();
         int size = s1.size();
-        while(!s1.isEmpty()){
+        while (!s1.isEmpty()) {
             temp = s1.pop();
             size--;
-            if(temp == key){
+            if (temp == key) {
                 flag = true;
                 break;
-            }else{
-               s2.push(temp);
+            } else {
+                s2.push(temp);
             }
             count++;
         }
-        while(!s2.isEmpty()){
+        while (!s2.isEmpty()) {
             s1.push(s2.pop());
         }
-        if(flag){
-            System.out.println(temp+" at : "+size+" in count : "+count);
+        if (flag) {
+            System.out.println(temp + " at : " + size + " in count : " + count);
             System.out.println(s1);
         }
     }
--- DSA_LAB/ASSIGNMENT_6/src/Question_2.java
+++ DSA_LAB/ASSIGNMENT_6/src/Question_2.java
@@ -7,14 +7,14 @@ public class Question_2 {
         int temp;
         stack.push(5);
         stack.push(4);
-        stack.push(3);
+        stack.push(4);
         stack.push(2);
         stack.push(1);
         System.out.println("Unsorted : ");
         System.out.println(stack);
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             temp = stack.pop();
-            while(!stack2.isEmpty() && stack2.peek() > temp){
+            while (!stack2.isEmpty() && stack2.peek() > temp) {
                 stack.push(stack2.pop());
             }
             stack2.push(temp);
