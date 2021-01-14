dcd340980d6f07df7e1e7c75658286ae4ae9ce33
jaffarabbas <gamaportal8@gmail.com>
2021-01-14 12:26:04 +0500

    1-14-2021

--- FINAL LAB PAPER PRACTICE/src/search.java
+++ FINAL LAB PAPER PRACTICE/src/search.java
@@ -1,11 +1,24 @@
 
 class BinarySearch{
-    int low,mid,high;
-    public void Search(int[] arr,int low,int high,int search){
-        mid = 
+    static int mid,count=0;
+    static boolean flag = false;
+    public static void Search(int[] arr,int low,int high,int search){
+        mid = (low+high)/2;
         do{
-
+            if(search == arr[mid]){
+                flag = true;
+                break;
+            }else if(arr[mid] < search){
+                low = mid+1;
+            }else if(arr[mid] > search){
+                high = mid-1;
+            }
+            mid = (low+high)/2 ;
+            count++;
         }while (low<=high);
+        if(flag){
+            System.out.println("value : "+arr[mid]+" in count : "+count);
+        }
     }
 }
 
@@ -13,6 +26,7 @@ class BinarySearch{
 
 public class search {
     public static void main(String[] args) {
-
+        int[] arr = {5,4,3,1,2};
+        BinarySearch.Search(arr,0, arr.length,3);
     }
 }
