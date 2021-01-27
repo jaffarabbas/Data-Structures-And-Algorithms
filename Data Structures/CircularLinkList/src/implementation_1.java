64d87ae02481961255be65c5d59b23b16a99c996
jaffarabbas <gamaportal8@gmail.com>
2021-01-27 23:54:33 +0500

    1-27-2021

---
+++ Data Structures/CircularLinkList/src/implementation_1.java
@@ -0,0 +1,19 @@
+
+
+class LinkedList{
+    class Node{
+        Node node;
+        int data;
+        Node(){
+            
+        }
+    }
+}
+
+
+
+public class implementation_1 {
+    public static void main(String[] args) {
+
+    }
+}
