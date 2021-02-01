418509b4508caa1057f8ce315cb21b7523982918
jaffarabbas <gamaportal8@gmail.com>
2021-02-01 19:48:03 +0500

    2-1-2021   Graph insertion by list (java)

---
+++ Data_practice_cross_language/java/Graphs/Adjcent_Graphs_List.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+
+class Graph {
+    int vertex;
+    LinkedList<Integer> list[];
+
+    public Graph(int vertex) {
+        this.vertex = vertex;
+        list = new LinkedList[vertex];
+        for (int i = 0; i <vertex ; i++) {
+            list[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int source, int destination){
+
+        //add edge
+        list[source].addFirst(destination);
+
+        //add back edge ((for undirected)
+        list[destination].addFirst(source);
+    }
+
+    public void printGraph(){
+        for (int i = 0; i <vertex ; i++) {
+            if(list[i].size()>0) {
+                System.out.print("Vertex " + i + " is connected to: ");
+                for (int j = 0; j < list[i].size(); j++) {
+                    System.out.print(list[i].get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+    }   
+}
+
+public class Adjcent_Graphs_List {
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addEdge(0,1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.printGraph();
+    }
+}
