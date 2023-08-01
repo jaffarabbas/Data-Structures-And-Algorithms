c6e291f57cc3b5a92f3dc05b05452b1d28c6ee62
jaffarabbas <gamaportal8@gmail.com>
2023-08-01 22:07:17 +0500

    Commited File : Program.cs at 01/08/2023 10:07:17 pm

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,29 +1,20 @@
 ﻿using DSA.CodeCheff;
+using DSA.Other;
 using System;
 
 namespace DSA // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        
         public static void Main()
         {
-            int n = Convert.ToInt32 (Console.ReadLine());
-            string v = n!=0 ? n.ToString() : new string("0");
-            string f = new string(v.Reverse().ToArray());
-            string final = "";
-            if(f.Last().Equals('-'))
+            //Console.WriteLine(numRoundtoFive.numRoundFor2and7(LastInteger.LastIntegerValue(102),true));
+            int[] arr = { 102, 102, 102, 102 };
+            foreach (int i in agiloSoftQuestion2.round(arr))
             {
-                final = f.Remove(f.Length-1);
-                final = "-"+final;
-            }else if (f[0].Equals('0'))
-            {
-                final = f.Remove(0,1);
+                Console.WriteLine(i);
             }
-            else
-            {
-                final = f;
-            }
-            Console.WriteLine(final);
         }
 
     }
