5b6a69f99cd56f93162f523d5e762333498e99c9
jaffarabbas <gamaportal8@gmail.com>
2023-08-01 22:07:13 +0500

    Commited File : LastInteger.cs at 01/08/2023 10:07:12 pm

---
+++ DSA/DSA/Other/LastInteger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Other
+{
+    public class LastInteger
+    {
+        public static int LastIntegerValue(int a)
+        {
+            return a % 10;
+        }
+    }
+}
