8ed5e1afa26eacead904c29c41d93fa1285b322d
jaffarabbas <gamaportal8@gmail.com>
2023-08-01 22:07:12 +0500

    Commited File : IsSemmetric.cs at 01/08/2023 10:07:12 pm

---
+++ DSA/DSA/Other/IsSemmetric.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Other
+{
+    sealed class IsSemmetric
+    {
+        public static bool isSummettic()
+        {
+            int[][] mar = { new[] { 1, 0, 0 }, new[] { 0, 1, 0 }, new[] { 0, 0, 1 } };
+            for (int i = 0; i < mar.Length; i++)
+            {
+                for (int j = i + 1; j < mar[i].Length; j++)
+                {
+                    Console.WriteLine(" i : " + i + " j : " + j);
+                    if (mar[i][j] != mar[j][i])
+                    {
+                        Console.WriteLine(mar[i][j] + " i : " + i + " j : " + j);
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
