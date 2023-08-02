11921acc751fa84005a495e26872209e999dc568
jaffarabbas <gamaportal8@gmail.com>
2023-08-02 20:47:58 +0500

    Commited File : PRACLIST.cs at 02/08/2023 8:47:57 pm

--- DSA/DSA/CodeCheff/PRACLIST.cs
+++ DSA/DSA/CodeCheff/PRACLIST.cs
@@ -8,5 +8,13 @@ namespace DSA.CodeCheff
 {
     internal class PRACLIST
     {
+        public static void p()
+        {
+            string[] lines = Console.ReadLine().Split(' ');
+            int x = Convert.ToInt32(lines[0]);
+            int y = Convert.ToInt32(lines[1]);
+            int f = x - y;
+            Console.WriteLine(f);
+        }
     }
 }
