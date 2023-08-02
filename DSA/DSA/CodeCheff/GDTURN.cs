9660e22249b7a787cf119394009d0e565f212b18
jaffarabbas <gamaportal8@gmail.com>
2023-08-02 19:16:53 +0500

    Commited File : GDTURN.cs at 02/08/2023 7:16:53 pm

--- DSA/DSA/CodeCheff/GDTURN.cs
+++ DSA/DSA/CodeCheff/GDTURN.cs
@@ -8,9 +8,25 @@ namespace DSA.CodeCheff
 {
     internal class GDTURN
     {
-        public static string isGood()
+        public static void isGood()
         {
-            return null;
+            int t = Convert.ToInt32(Console.ReadLine());
+            while (t > 0)
+            {
+                string[] v = Console.ReadLine().Split(" ");
+                int a = Convert.ToInt32(v[0]);
+                int b = Convert.ToInt32(v[1]);
+
+                if((a + b) > 6)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+                t--;
+            }
         }
     }
 }
