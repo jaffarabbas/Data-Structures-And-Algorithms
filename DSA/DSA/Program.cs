7639b8d6b68d48913984a911738191542f9ac05b
jaffarabbas <gamaportal8@gmail.com>
2023-08-02 19:16:54 +0500

    Commited File : Program.cs at 02/08/2023 7:16:54 pm

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -9,12 +9,8 @@ namespace DSA // Note: actual namespace depends on the project name.
         
         public static void Main()
         {
-            //Console.WriteLine(numRoundtoFive.numRoundFor2and7(LastInteger.LastIntegerValue(102),true));
-            int[] arr = { 102, 102, 102, 102 };
-            foreach (int i in agiloSoftQuestion2.round(arr))
-            {
-                Console.WriteLine(i);
-            }
+            AGELIMIT.age
+                ();
         }
 
     }
