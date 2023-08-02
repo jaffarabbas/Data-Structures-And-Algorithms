c4bbad6b318098a1e6d5f3f5e90c0ac1008dc09c
jaffarabbas <gamaportal8@gmail.com>
2023-08-02 19:51:03 +0500

    Commited File : BURGERS.cs at 02/08/2023 7:51:02 pm

---
+++ DSA/DSA/CodeCheff/BURGERS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff
+{
+    internal class BURGERS
+    {
+        public static void b()
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            while (t > 0)
+            {
+                string[] lines = Console.ReadLine().Split(' ');
+                int x = Convert.ToInt32(lines[0]);
+                int y = Convert.ToInt32(lines[1]);
+                if(x == y)
+                {
+                    Console.WriteLine(x);
+                }else if (x < y)
+                {
+                    Console.WriteLine(x);
+                }
+                else
+                {
+                    Console.WriteLine(y);
+                }
+                t--;
+            }
+        }
+    }
+}
