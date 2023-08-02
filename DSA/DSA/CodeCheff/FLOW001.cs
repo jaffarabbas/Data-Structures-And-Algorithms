942c87c3ea126799797932dfca69ee881faab87d
jaffarabbas <gamaportal8@gmail.com>
2023-08-02 19:16:51 +0500

    Commited File : FLOW001.cs at 02/08/2023 7:16:51 pm

---
+++ DSA/DSA/CodeCheff/FLOW001.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff
+{
+    internal class FLOW001
+    {
+        public static void add()
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                string[] lines = Console.ReadLine().Split(' ');
+                Console.WriteLine(int.Parse(lines[0]) + int.Parse(lines[1]));
+            }
+        }
+    }
+}
