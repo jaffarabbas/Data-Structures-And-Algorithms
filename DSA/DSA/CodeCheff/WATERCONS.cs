d75bd56661d068016b2c4ecf54d98eca63a30c56
jaffarabbas <gamaportal8@gmail.com>
2023-08-02 19:16:52 +0500

    Commited File : WATERCONS.cs at 02/08/2023 7:16:52 pm

---
+++ DSA/DSA/CodeCheff/WATERCONS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff
+{
+    internal class WATERCONS
+    {
+        public static void water()
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            while (t > 0)
+            {
+                int y = Convert.ToInt32(Console.ReadLine());
+                if(y >= 2000)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+                t--;
+            }
+        }
+    }
+}
