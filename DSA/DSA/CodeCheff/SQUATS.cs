6c926582f769e602171b2abdd17e8fdb0b4f84d4
jaffarabbas <gamaportal8@gmail.com>
2023-08-02 19:16:52 +0500

    Commited File : SQUATS.cs at 02/08/2023 7:16:51 pm

---
+++ DSA/DSA/CodeCheff/SQUATS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff
+{
+    internal class SQUATS
+    {
+        public static void squarts()
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            while (t > 0)
+            {
+                int v = Convert.ToInt32(Console.ReadLine());
+                int f = v * 15;
+                Console.WriteLine(f);
+                t--;
+            }
+        }
+    }
+}
