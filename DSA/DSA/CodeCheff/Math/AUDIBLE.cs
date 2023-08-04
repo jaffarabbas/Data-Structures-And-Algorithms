423bf895f69efc63d5e3ebd13b27ea221cffd7fb
jaffarabbas <gamaportal8@gmail.com>
2023-08-04 12:12:38 +0500

    Commited File : AUDIBLE.cs at 04/08/2023 12:12:38 pm

---
+++ DSA/DSA/CodeCheff/Math/AUDIBLE.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Math
+{
+    internal class AUDIBLE
+    {
+        public static void func()
+        {
+            int a = int.Parse(Console.ReadLine());
+            while (a > 0)
+            {
+                
+                int x = int.Parse(Console.ReadLine());
+             
+                if (x >= 67 && x <= 45000)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+                a--;
+            }
+        }
+    }
+}
