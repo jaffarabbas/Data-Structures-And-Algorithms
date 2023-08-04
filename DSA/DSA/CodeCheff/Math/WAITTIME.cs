eab5e22d057a4b930135406f3f22d20f7bcd7a44
jaffarabbas <gamaportal8@gmail.com>
2023-08-04 12:12:38 +0500

    Commited File : WAITTIME.cs at 04/08/2023 12:12:38 pm

---
+++ DSA/DSA/CodeCheff/Math/WAITTIME.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Math
+{
+    internal class WAITTIME
+    {
+        public static void func()
+        {
+            int a = int.Parse(Console.ReadLine());
+            while (a > 0)
+            {
+                string[] lines = Console.ReadLine().Split(' ');
+                int x = int.Parse(lines[0]);
+                int y = int.Parse(lines[1]);
+                int w = x * 7;
+                int f = w - y;
+                Console.WriteLine(f);
+                a--;
+            }
+        }
+    }
+}
